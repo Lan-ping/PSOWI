f38eaee0160d4585e9d0c4c792841d35565d6e57
xd_zhiwang <xd_zhiwang@163.com>
2022-03-03 19:38:53 +0800

    release v1.0.2

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("README.md", "r", encoding='utf-8') as fh:
  
 setuptools.setup(
     name='psowi',  # 模块名称
-    version="1.0.1",  # 当前版本
+    version="1.0.2",  # 当前版本
     author="lanping_tech",  # 作者
     author_email="lanping_tech@qq.com",  # 作者邮箱
     description="Keras Model Weight Initialization By PSO",  # 模块简介
     long_description=long_description,  # 模块详细介绍
     long_description_content_type="text/markdown",  # 模块详细介绍格式
-    url="https://github.com/Lan-ping/PSOWI",  # 模块github地址
+    url="https://github.com/Lanping-Tech/PSOWI",  # 模块github地址
     packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
     # 模块相关的元数据
     classifiers=[
